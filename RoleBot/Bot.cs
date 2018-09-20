00eeb5ffb8cc0b200007f821f63a09883d75abb1
Shadow <cam.v737@gmail.com>
2018-09-20 12:48:17 -0400

    Dynamic Assigning Complete - no known bugs

---
+++ RoleBot/Bot.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+
+namespace RoleBot
+{
+	internal class Bot
+	{
+		internal static DiscordClient Client { get; private set; }
+		private static CommandsNextModule Commands { get; set; }
+		private static DiscordMessage TargetMessage { get; set; }
+		internal static DiscordChannel TargetChannel { get; private set; }
+		internal static List<DiscordRole> RolesToAssign { get; set; } // split from configuration file
+		internal static List<DiscordEmoji> EmojisToAssign { get; set; } // split from configuration file
+
+		internal static async Task RunBotAsync()
+		{
+			var clientConfig = (new DiscordConfiguration
+			{
+				AutoReconnect = true,
+				Token = ConfigurationManager.AppSettings.Get("token"),
+				TokenType = TokenType.Bot,
+
+				LogLevel = LogLevel.Debug,
+				UseInternalLogHandler = true
+			});
+
+			// instantiated the client
+			Client = new DiscordClient(clientConfig);
+			
+			// logs before bot is live
+			Client.Ready += LogPrinter.Client_Ready;
+			Client.GuildAvailable += LogPrinter.Guild_Available;
+			Client.ClientErrored += LogPrinter.Client_Error;
+
+			var commandsConfig = new CommandsNextConfiguration
+			{
+				// sets the prefix for messages to be treated as commands
+				StringPrefix = ConfigurationManager.AppSettings.Get("commandPrefix"),
+				// sets the ability of the bot to send DMS to people in the server
+				EnableDms = true,
+				// allows Bot-Mention to be a valid command prefix
+				EnableMentionPrefix = true
+			};
+
+			// connects discord client to commands module
+			Commands = Client.UseCommandsNext(commandsConfig);
+
+			// making Console a little more lively with debug-style
+			Commands.CommandExecuted += LogPrinter.Commands_CommandExecuted;
+			Commands.CommandErrored += LogPrinter.Commands_CommandError;
+			
+			// Register the commands for usage with RoleBot
+			Commands.RegisterCommands<Commands>();
+			Commands.SetHelpFormatter<HelpFormatter>();
+
+			// Set target Channel and message to track through ReactionRole duties
+			TargetChannel = Client.GetChannelAsync(ulong.Parse(ConfigurationManager.AppSettings.Get("targetChannel")))
+				.Result;
+			TargetMessage = TargetChannel
+				.GetMessageAsync(ulong.Parse(ConfigurationManager.AppSettings.Get("targetMessage"))).Result;
+
+			Client.MessageReactionAdded += Reaction_Added;
+			Client.MessageReactionRemoved += Reaction_Removed;
+			
+			await Client.ConnectAsync();
+			await Task.Delay(-1);
+		}
+
+		private static async Task Reaction_Added(MessageReactionAddEventArgs e)
+		{
+			if (e.Message.Equals(TargetMessage))
+			{
+				var guild = TargetChannel.Guild;
+				
+				// all members who reacted
+				var membersReacted = from discordUser in TargetMessage.GetReactionsAsync(e.Emoji).Result
+					select guild.GetMemberAsync(discordUser.Id).Result;
+
+				// Grants roles retroactively through the use of a switch based on the emote used
+				foreach (var member in membersReacted)
+				{
+					for (var i = 0; i < EmojisToAssign.Count; i++)
+					{
+						if (e.Emoji.Equals(EmojisToAssign[i]))
+							await member.GrantRoleAsync(RolesToAssign[i]);
+					}
+				}
+			}
+		}
+
+		private static async Task Reaction_Removed(MessageReactionRemoveEventArgs e)
+		{
+			if (e.Message.Equals(TargetMessage))
+			{
+				var guild = TargetChannel.Guild;
+				
+				// retro actively tries to remove roles (created in case bot goes offline)
+				var guildMembers = guild.GetAllMembersAsync().Result;
+
+				var membersReacted = from discordUser in TargetMessage.GetReactionsAsync(e.Emoji).Result
+					select guild.GetMemberAsync(discordUser.Id).Result;
+
+				// filters members to remove
+				var membersToRemove = from discordMember in guildMembers
+					where !membersReacted.Contains(discordMember)
+					select discordMember;
+				
+				// retroactively removes roles
+				foreach (var member in membersToRemove)
+				{
+					for (var i = 0; i < EmojisToAssign.Count; i++)
+						if (e.Emoji.Equals((EmojisToAssign[i])))
+							await member.RevokeRoleAsync(RolesToAssign[i]);
+				}
+			}
+		}
+	}
+}
--- RoleBot/Commands.cs
+++ RoleBot/Commands.cs
@@ -9,7 +9,7 @@ using DSharpPlus.CommandsNext.Attributes;
 
 namespace RoleBot
 {
-    public class Commands
+    internal class Commands
     {
         [Command("mention"), Description("Calls out the specified user"), Aliases("fetch", "hail")]
         public async Task Mention(CommandContext ctx, [Description("Calls out the specified user")]
@@ -17,16 +17,16 @@ namespace RoleBot
         {
             await ctx.TriggerTypingAsync();
 
-            if (member.Equals(Program.Client.GetUserAsync(397454954757095424).Result))
+            if (member.Equals(Bot.Client.GetUserAsync(397454954757095424).Result))
                 await ctx.RespondAsync($"Get your ass down here Ni:b::b:a {member.Mention}");
 
-            else if (member.Equals(Program.Client.GetUserAsync(251505256662302731).Result))
+            else if (member.Equals(Bot.Client.GetUserAsync(251505256662302731).Result))
                 await ctx.RespondAsync($"Fetch Reinforcements {member.Mention}");
 
-            else if (member.Equals(Program.Client.GetUserAsync(446831227450949656).Result))
+            else if (member.Equals(Bot.Client.GetUserAsync(446831227450949656).Result))
                 await ctx.RespondAsync($"Leave your laptop outside {member.Mention}");
             
-            else if (member.Equals(Program.Client.GetUserAsync(400798130091851776).Result))
+            else if (member.Equals(Bot.Client.GetUserAsync(400798130091851776).Result))
                 await ctx.RespondAsync($"We need a whale with dwarfism {member.Mention}");
 
             else
@@ -39,7 +39,7 @@ namespace RoleBot
             DiscordMember member)
         {
             await ctx.TriggerTypingAsync();
-            String[] synonyms =
+            string[] synonyms =
             {
                 "bargain",
                 "wager",
